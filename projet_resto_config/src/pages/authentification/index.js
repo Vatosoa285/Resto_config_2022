b931be80bdfb4b212c35a915d224372300680fa1
“Vatosoa” <vats@DESKTOP-3HG2R6V.localdomain>
2022-07-11 14:57:08 +0200

    début lien serveur

--- projet_resto_config/src/pages/authentification/index.js
+++ projet_resto_config/src/pages/authentification/index.js
@@ -1,10 +1,133 @@
+/*import React, {useRef } from 'react';
+import '../../Style/Login.css'; 
+import { Button, Text, View, Image, TextInput, Pressable, ActivityIndicator} from 'react-native';
+
+import {userContext} from '../../contexts/userContext';
+import {sendAPI} from '../../Components/adapter_functions/sendAPI';
+
+import * as Device from 'expo-device';
+import * as SecureStore from 'expo-secure-store';
+
+import constantes from '../../constantes';
+import styles from '../../styles';
+import logo from '../../assets/logoRdC.png';
+
+
+export default function Login({navigation}) {
+
+	// On récupère la fonction pour gérer le token de notification
+	const registerForPushNotificationsAsync = React.useContext(userContext).registerForPushNotificationsAsync;
+
+	const refMdp = useRef(null);
+	const navigate = useNavigate
+
+	const [textEmail, onChangeTextEmail] = React.useState('');
+	const [textPassword, onChangeTextPassword] = React.useState('');
+
+	const [loading, setLoading] = React.useState(false);
+	
+	const changeID = React.useContext(userContext).changeID;
+	const changeToken = React.useContext(userContext).changeToken;
+	
+    //Handler des erreurs de serveur
+    const handleError = React.useContext(userContext).handleError;
+  
+  function checkPassword() {
+	if (textEmail != '' && textPassword != '') {
+		setLoading(true);
+		sendAPI('AUT', 'AP_LOGIN', {'email':textEmail, 'motDePasse':textPassword})	
+		.then(async function (data){
+			
+			const device = await Device.getDeviceTypeAsync();
+			const token = await registerForPushNotificationsAsync(device);
+			
+			//On n'envoie le token de notification que s'il est différent de celui stocké sur le serveur
+			if (data.tokennotification != token && token != "-1"){
+				sendAPI('APP', 'AP_UPD_NOTIF', {'P_IDBENEVOLE':data.id, 'P_TOKENNOTIF':token}, data.token)
+				.catch((error) => {handleError(error); setLoading(false)});
+			}
+			login(data);
+		})
+		.catch((error) => {handleError(error); setLoading(false)})
+		  
+		onChangeTextPassword(''); //On vide le champ mot de passe
+	}
+  }
+  
+  
+  function login(data) {
+	  if (Device.brand){
+		SecureStore.setItemAsync('id', data.id);
+		SecureStore.setItemAsync('token', data.token);
+	  }
+	  changeToken(data.token);
+	  changeID(data.id);
+    }
+
+  return (
+    <>
+    <View style={[styles.container,{alignItems: "center"}]}>
+	  <Image
+		style={styles.logo}
+        source={logo}
+      />
+      <Text style={styles.idTexte}>Adresse mail</Text>
+	  <TextInput
+		style={styles.idInput}
+		placeholder="votre@email.fr"
+		onChangeText={onChangeTextEmail}
+		keyboardType = "email-address"
+		textContentType = "emailAddress"
+		autoCompleteType = "email"
+		autoCorrect = {false}
+		onSubmitEditing={() => refMdp.current.focus()}
+	  />
+	  <Text style={styles.idTexte}>Mot de passe</Text>
+	  <TextInput
+	  	ref={refMdp}
+		style={styles.idInput}
+		placeholder="********"
+		secureTextEntry = {true}
+        onChangeText={onChangeTextPassword}
+		value={textPassword}
+		textContentType = "password"
+		onSubmitEditing={() => checkPassword()}
+
+	  />
+	  <Button
+		onPress = {() => checkPassword()} //{() => navigation.navigate('Engagements')}
+		title="CONNEXION"
+		color="#e7007d"
+		accessibilityLabel="Bouton de connexion"
+		testID="LoginButton"
+
+	  />
+	  <Pressable title = "mdp" onPress = {() => navigate("/oublie")} >
+		<Text style={styles.idTexte}>Mot de passe oublié ?</Text>
+	  </Pressable>
+    </View>
+	{loading &&
+    <View style={styles.loading}>
+      <ActivityIndicator size="large" color="#e92682" />
+    </View>
+	}
+    </>
+  );
+}   
+*/
+
+
 import '../../Style/Login.css'
 import '../../Style/Button.css'
 import logo from "../../assets/logo.svg"
-import {useState, useEffect} from 'react'
+import {React, useState, useEffect} from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
+import {userContext} from '../../contexts/userContext';
+import {sendAPI} from '../../Components/adapter_functions/sendAPI';
 
 
+import * as Device from 'expo-device';
+import * as SecureStore from 'expo-secure-store';
 
 
 function Login() {
@@ -15,12 +138,31 @@ function Login() {
 
 
   const navigate = useNavigate();
+  const registerForPushNotificationsAsync = React.useContext(userContext).registerForPushNotificationsAsync;
+
+  function checkPassword() {
+		sendAPI('AUT', 'AP_LOGIN', {'email':'amauryalric@gmail.com', 'motDePasse':'Rdc-3131'})	
+		.then(async function (data){
+			console.log(data);
+
+			const device = await Device.getDeviceTypeAsync();
+			const token = await registerForPushNotificationsAsync(device);
+
+		})
+		.catch((error) => {
+			console.log(error);
+		})
+		  
+	}
 
   // Verifie que les identifiants sont valides avant de mettre submit à true
   const handleSubmit = (err) => {
         err.preventDefault();
        if (validate()) { setIsSubmit(true);
-      }   
+      }  
+	  console.log("hola");
+	  checkPassword();
+
     }
   
     // Conditions sur les identifiants
